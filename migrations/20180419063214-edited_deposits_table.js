da59d2460ca9b19d53a38c773945b227d72c55da
Nilesh <nilesh@wrctpl.com>
2018-04-19 12:05:50 +0530

    Nilesh

---
+++ migrations/20180419063214-edited_deposits_table.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return [
+      queryInterface.changeColumn(
+        'deposits',
+        'payment_method',
+        {
+          type: Sequelize.INTEGER(1),
+          allowNull: false
+        }
+      )
+    ];
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};

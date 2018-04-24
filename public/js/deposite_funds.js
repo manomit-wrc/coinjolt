a01b10b155987bd7ce97e6a8d50882c3c9a0c0e4
sobhanwrc <sobhan@wrctpl.com>
2018-04-24 14:35:24 +0530

    commit by sobhan

--- public/js/deposite_funds.js
+++ public/js/deposite_funds.js
@@ -1,6 +1,14 @@
 $(document).ready(function (e) {
 	$("#showing_warning").hide();
 
+	$( ".labelsetfield" ).focus(function() {
+		$(".labelset").show();
+		var classname = $(this).attr('name');
+		var showclassname = "."+classname;
+		$(showclassname).show();
+		$(this).css("padding-top", "22px");
+	});
+
 	$('#usd_amount').on('keypress', function (event) {
 		var charCode = (event.which) ? event.which : event.keyCode
 		if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57))

f7133bcea2545dd48cbf09699f5f9c48d11bde65
Nilesh <nilesh@wrctpl.com>
2018-05-03 18:59:06 +0530

    NILESH

--- migrations/20180503130612-create-answer.js
+++ migrations/20180503130612-create-answer.js
@@ -2,7 +2,7 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('Answers', {
-      id: {
+      id: { 
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,

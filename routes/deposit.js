923dcbabd250bfa84b211a49d23824e028f10037
Nilesh <nilesh@wrctpl.com>
2018-05-24 15:46:23 +0530

    Dashboard page responsive

--- routes/deposit.js
+++ routes/deposit.js
@@ -106,7 +106,7 @@ module.exports = function(app,Deposit,WireTransfer,User,Referral_data,Currency,C
 				}).then (function (result) {
 					res.json({
 	                    status: true,
-	                    message: 'Approved and deposilt succesfully.'
+	                    message: 'Approved and deposit succesfully.'
 	                });
 				});
 			}

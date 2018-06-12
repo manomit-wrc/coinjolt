01753efbaae38f729fb2efab0c9ac18a4936929d
Nilesh <nilesh@wrctpl.com>
2018-06-12 10:48:38 +0530

    Terms of service added

--- routes/cms.js
+++ routes/cms.js
@@ -1,21 +1,22 @@
-
+const sequelize = require('sequelize');
 module.exports = function (app, models) {
     app.get('/admin/cms/terms-of-service', (req, res) =>{
 
         models.cms_terms_of_service.find({}).then(function(terms_of_service){
-            // fetch prev. inserted rec id , then load data
             res.render('admin/cms/terms_of_service',{layout: 'dashboard', terms_of_service: terms_of_service});
         });
     });
 
     app.post('/admin/submit-terms-of-service', (req, res) =>{
 
-        models.cms_terms_of_service.find({}).then(function(results){
-            var terms_id = results.id;
-            //console.log('Terms id: ', terms_id);
+        models.cms_terms_of_service.findAndCountAll({
+            order: [
+                sequelize.fn('max', sequelize.col('id'))
+            ]
+        }).then(function(results){
+            var terms_id = results.rows[0].id;
             var count = results.count;
             if(count >0){
-                
                 models.cms_terms_of_service.update({
                     terms_of_service_header_desc: req.body.banner_image_title,
                     terms_of_service_content: req.body.terms_of_service_description

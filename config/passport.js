830dd48792c1229cb02e382e63007e989b1c7e83
Nilesh <nilesh@wrctpl.com>
2018-07-13 14:23:08 +0530

    Email tmplt modified

--- config/passport.js
+++ config/passport.js
@@ -386,7 +386,7 @@ module.exports = (passport, User, Deposit, Currency, models, AWS) => {
                                             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;" >
                                               <tr>
                                                 <td bgcolor="#ffffff" align="center" valign="top" style="padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: 'AvenirNextLTPro-Regular', sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;">
-                                                  <h1 style="font-size: 48px; font-weight: bold; margin: 0;">Welcome!</h1>
+                                                  <h1 style="font-size: 48px; font-weight: bold; margin: 0;"></h1>
                                                 </td>
                                               </tr>
                                             </table>
@@ -718,7 +718,7 @@ module.exports = (passport, User, Deposit, Currency, models, AWS) => {
                                             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;" >
                                               <tr>
                                                 <td bgcolor="#ffffff" align="center" valign="top" style="padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: 'AvenirNextLTPro-Regular', sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;">
-                                                  <h1 style="font-size: 48px; font-weight: bold; margin: 0;">Welcome!</h1>
+                                                  <h1 style="font-size: 48px; font-weight: bold; margin: 0;"></h1>
                                                 </td>
                                               </tr>
                                             </table>
